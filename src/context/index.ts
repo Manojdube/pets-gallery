d5251102a7a8c0c9be9d7017ae831146513d33af
Manoj Dube <manojdube1996@gmail.com>
2026-04-21 03:19:24 +0530

    clean up

--- src/context/index.ts
+++ src/context/index.ts
@@ -4,11 +4,8 @@ export { usePetsData } from "./petsData/usePetsData";
 
 export { SelectionProvider } from "../context/petSelection/SelectionProvider";
 export { useSelectionActions, useSelectionState, useSelection } from "./petSelection/useSelection";
-export type { SelectionActionsType, SelectionStateType, UseSelectionType } from "./petSelection/selectionTypes";
 
 export { PetDetailProvider } from "./petDetail/PetDetailProvider";
 export { usePetDetail } from "./petDetail/usePetDetail";
-export type { PetDetailContextType } from "./petDetail/PetDetailContexts";
 
 export { PetsDataProvider } from "./petsData/PetsDataProvider";
-export type { PetsDataContextType } from "./petsData/PetsDataProvider";
